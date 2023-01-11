7d4c7c15a7f402466ae4f29bb8f67bade28db9c3
Kamil Boryczka <leoctaneee@gmail.com>
2023-01-11 22:56:23 +0100

    Better app navigation

--- lib/src/main_nav.dart
+++ lib/src/main_nav.dart
@@ -1,61 +1,72 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:ionicons/ionicons.dart';
-import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:travy/src/common.dart';
 import 'package:travy/src/pages/home_page.dart';
 
-class MainNav extends StatelessWidget {
+class MainNav extends StatefulWidget {
   const MainNav({super.key});
 
   @override
+  State<MainNav> createState() => _MainNavState();
+}
+
+class _MainNavState extends State<MainNav> {
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return PersistentTabView(
-      context,
-      screens: _getScreens,
-      items: _getItems,
-      screenTransitionAnimation: const ScreenTransitionAnimation(
-        animateTabTransition: true,
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: const [
+          HomePage(),
+          Scaffold(),
+          Scaffold(),
+          Scaffold(),
+        ],
+      ),
+      extendBody: true,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        onTap: (index) => setState(() => currentIndex = index),
+        index: currentIndex,
+        animationDuration: const Duration(milliseconds: 300),
+        items: [
+          _buildItem(
+            index: 0,
+            icon: Ionicons.home_outline,
+            activeIcon: Ionicons.home,
+          ),
+          _buildItem(
+            index: 1,
+            icon: Ionicons.heart_outline,
+            activeIcon: Ionicons.heart,
+          ),
+          _buildItem(
+            index: 2,
+            icon: Ionicons.compass_outline,
+            activeIcon: Ionicons.compass,
+          ),
+          _buildItem(
+            index: 3,
+            icon: Ionicons.settings_outline,
+            activeIcon: Ionicons.settings,
+          ),
+        ],
       ),
     );
   }
 
-  List<PersistentBottomNavBarItem> get _getItems {
-    return [
-      _buildItem(
-        icon: const Icon(Ionicons.home_outline),
-        text: 'Home',
-      ),
-      _buildItem(
-        icon: const Icon(Ionicons.heart_outline),
-        text: 'Favorid',
-      ),
-      _buildItem(
-        icon: const Icon(Ionicons.compass_outline),
-        text: 'Destinations',
-      ),
-      _buildItem(
-        icon: const Icon(Ionicons.settings_outline),
-        text: 'Settings',
-      ),
-    ];
-  }
-
-  List<Widget> get _getScreens {
-    return const [
-      HomePage(),
-      Scaffold(),
-      Scaffold(),
-      Scaffold(),
-    ];
-  }
-
-  PersistentBottomNavBarItem _buildItem({
-    required Widget icon,
-    required String text,
+  Widget _buildItem({
+    required int index,
+    required IconData icon,
+    required IconData activeIcon,
   }) {
-    return PersistentBottomNavBarItem(
-      icon: icon,
-      title: text,
-      inactiveColorPrimary: Colors.grey.shade500,
-    );
+    return currentIndex == index
+        ? Icon(
+            activeIcon,
+            color: Colors.blue,
+          )
+        : Icon(icon);
   }
 }
